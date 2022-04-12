f0a597ce09024ac6971dc2c934aaf41938408ae3
jhonny oliveira <jhonnyoliv3ira@gmail.com>
2022-04-12 13:06:09 -0300

    commit 1

---
+++ src/main/java/com/maMovie/MaMovieApplication.java
@@ -0,0 +1,13 @@
+package com.maMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maMovie/MaMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
